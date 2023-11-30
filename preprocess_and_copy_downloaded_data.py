a122a5158127b42b04e96169ae12afaabb7eb19b
Nils Cercariolo <nils.cercariolo@gmail.com>
2023-12-01 00:48:35 +0100

    fix typo

--- preprocess_and_copy_downloaded_data.py
+++ preprocess_and_copy_downloaded_data.py
@@ -114,7 +114,7 @@ def create_species_dict(input_file, species_counts, occurences_threshold, max_lo
         for year in years:
             year_rows = [
                 row for row in rows if row["date"].split("-")[0] == year]
-            print(rows_per_species.keys())
+            print(years)
             total_species_occurrences_per_year = len(year_rows)
 
             # Determine the appropriate max value based on the year
